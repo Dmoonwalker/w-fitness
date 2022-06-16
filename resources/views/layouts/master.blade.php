e7fb6cc1202adb860290a2fdf771cec10b273c95
unknown <fadlullahbugaje@gmail.com>
2022-06-16 05:50:24 -0700

    v1.2.1

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -51,15 +51,6 @@
         <nav id="sidebarMenu" class="sidebar d-lg-block bg-gray-800 text-white collapse" data-simplebar>
   <div class="sidebar-inner px-4 pt-3">
     
-        <div class="collapse-close d-md-none">
-           <a href="#sidebarMenu" data-bs-toggle="collapse"
-            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="true"
-            aria-label="Toggle navigation">
-            <svg class="icon icon-xs" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-          </a>
-        
-        </div>
-    
     <ul class="nav flex-column pt-3 pt-md-0">
       <li class="nav-item">
         <a href="#" class="nav-link d-flex align-items-center">
@@ -135,7 +126,7 @@
     </ul>
   </div >
 
-  /
+  
 </nav>
     
 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
